61ffe8e23e3d1492ef09dbe65074bd0f005e78c8
Pieromc <piero0716.mc@gmail.com>
2022-04-03 14:08:11 -0500

    styles side-bar

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,73 +1,78 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-side-bar',
-  templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.css']
+	selector: 'app-side-bar',
+	templateUrl: './side-bar.component.html',
+	styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent implements OnInit {
 
 
-   mainMenu : Array<any> = [];
-   libraryMenu : Array<any> = [];
-   playlistMenu : Array<any> = [];
+	mainMenu: Array<any> = [];
+	libraryMenu: Array<any> = [];
+	playlistMenu: Array<any> = [];
 
+	isToggled: String = 'close' || '';
 
+	constructor() { }
 
-  constructor() { }
+	ngOnInit(): void {
 
-  ngOnInit(): void {
+		this.mainMenu = [
+			{
 
-	this.mainMenu = [
-		{
-		
-			name: 'Discover',
-			icon: 'fas fa-th-large',
-			router: ['/']
-		},
-		{
-			name: 'Genres',
-			icon: 'fas fa-volume-up',
-			router: ['/', 'genres']
-		},
-		{
-			name: 'Albums',
-			icon: 'fas fa-compact-disc',
-			router: ['/', 'albums']
-		},
-		{
-			name: 'Artists',
-			icon: 'fas fa-microphone',
-			router: ['/', 'artists']
-		},
-	];
+				name: 'Discover',
+				icon: 'fas fa-th-large',
+				router: ['/']
+			},
+			{
+				name: 'Genres',
+				icon: 'fas fa-volume-up',
+				router: ['/', 'genres']
+			},
+			{
+				name: 'Albums',
+				icon: 'fas fa-compact-disc',
+				router: ['/', 'albums']
+			},
+			{
+				name: 'Artists',
+				icon: 'fas fa-microphone',
+				router: ['/', 'artists']
+			},
+		];
 
-	this.libraryMenu = [
-		{
-			name: 'Recent',
-			icon: 'fas fa-clock',
-			router: ['/', 'library', 'recent']
-		},
-		{
-			name: 'Favorites',
-			icon: 'fas fa-heart',
-			router: ['/', 'library', 'favorites']
-		},
-		{
-			name: 'Playlists',
-			icon: 'fas fa-list',
-			router: ['/', 'library', 'playlists']
-		}
+		this.libraryMenu = [
+			{
+				name: 'Recent',
+				icon: 'fas fa-clock',
+				router: ['/', 'library', 'recent']
+			},
+			{
+				name: 'Favorites',
+				icon: 'fas fa-heart',
+				router: ['/', 'library', 'favorites']
+			},
+			{
+				name: 'Playlists',
+				icon: 'fas fa-list',
+				router: ['/', 'library', 'playlists']
+			}
 
-	];
+		];
 
-	this.playlistMenu = [
-		{
-			name: 'Create new',
-			icon: 'fas fa-plus-circle',
-			router: ['/', 'playlist', 'create']
-		}
-	];
-  }
+		this.playlistMenu = [
+			{
+				name: 'Create new',
+				icon: 'fas fa-plus-circle',
+				router: ['/', 'playlist', 'create']
+			}
+		];
+	}
+
+	toggleSideBar() {
+		this.isToggled =
+			this.isToggled === 'close' ? '' : 'close';
+	}
 
 }
